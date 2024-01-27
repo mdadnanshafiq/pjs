37aa44b5eacafdc683aa4bec1c3b4bdc82a7c23d
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-01-28 03:33:43 +0600

    ternary done

---
+++ if-else/ternary.js
@@ -0,0 +1,16 @@
+let num1 = 10;
+let num2 = 50;
+
+let result;
+let result2;
+
+if (num1 > num2) {
+    result = num1 *2;
+} else {
+    result = num1 + num2;
+}
+
+num1 > num2 ? result2 = num1 *2 : result2 = num1 + num2;
+
+console.log(result);
+console.log(result2);
