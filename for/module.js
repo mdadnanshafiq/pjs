6c93363502f0026272ee54444d731737a63148e9
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-01-30 15:15:47 +0600

    module start done

---
+++ for/module.js
@@ -0,0 +1,9 @@
+let process =['unlock', 'watch', 'practice', 'note', 'practice', 'support'];
+
+// for(i = 0; i < process.length; i++) {
+//     console.log(process[i]);
+// }
+
+for(i = process.length - 1; i >= 0; i--) {
+    console.log(process[i]);
+}
---
+++ while/module.js
@@ -0,0 +1,12 @@
+let process = ['unlock', 'watch', 'practice', 'note', 'practice', 'support'];
+// let i = 0;
+// while (i < process.length) {
+//     console.log(process[i]);
+//     i++;
+// }
+
+let i = process.length -1;
+while (i >= 0) {
+    console.log(process[i]);
+    i--;
+}
