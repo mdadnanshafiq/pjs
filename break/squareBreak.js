21510fa7c0451dcf00d77ea14d5f49681a2a700d
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-01-30 19:03:13 +0600

    test

---
+++ break/squareBreak.js
@@ -0,0 +1,16 @@
+let arr = [];
+let sum = 0;
+let i = 1;
+
+while (i >= 1) {
+    arr.push(i)
+    sum += i;
+    console.log(i);
+    if () {
+        break;
+    }
+    i++;
+}
+
+console.log(arr);
+console.log(sum);
