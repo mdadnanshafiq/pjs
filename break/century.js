8c2391203fac5fbde09433c8674884c05d860017
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-01-30 17:27:20 +0600

    century done

---
+++ break/century.js
@@ -0,0 +1,16 @@
+for (i = 1; i <= 200; i++) {
+    console.log(i);
+    if (i === 100) {
+        break;
+    }
+}
+
+let n = 1;
+
+while (n <= 200) {
+    console.log(n);
+    if (n === 100) {
+        break;
+    }
+    n++;
+}
