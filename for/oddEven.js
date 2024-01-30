094c0e65ef1c451160df6cd54978dbb994723938
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-01-30 15:38:10 +0600

    oddEven done

---
+++ for/oddEven.js
@@ -0,0 +1,15 @@
+let odds =[];
+
+for (i = 61; i<=100; i+=2) {
+    odds.push(i);
+}
+
+console.log(odds);
+
+let evens =[];
+
+for (i = 78; i<=98; i+=2) {
+    evens.push(i);
+}
+
+console.log(evens);
---
+++ while/oddEven.js
@@ -0,0 +1,19 @@
+let odds =[];
+let i= 61;
+
+while (i<=100) {
+    odds.push(i);
+    i+=2;
+}
+
+console.log(odds);
+
+let evens =[];
+let n = 78;
+
+while (n<=98) {
+    evens.push(n);
+    n+=2;
+}
+
+console.log(evens);
