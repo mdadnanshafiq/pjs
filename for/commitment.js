9224f7d964b6797f437d5b7dd84ced794aed9af3
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-01-30 15:23:50 +0600

    commitment done

---
+++ for/commitment.js
@@ -0,0 +1,7 @@
+let commit = 'I will invest at least six hours every single day for next sixty days!';
+let count = 60;
+
+for (i = 1; i <= count; i++){
+    console.log(i);
+    console.log(commit);
+}
---
+++ while/commitment.js
@@ -0,0 +1,9 @@
+let commit = 'I will invest at least six hours every single day for next sixty days!';
+let count = 60;
+let i =1;
+
+while (i <= count){
+    console.log(i);
+    console.log(commit);
+    i++;
+}
