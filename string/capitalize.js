2ce4b1339d917947539c4e5ebaa6ef2eb25b6a83
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-02-01 02:49:28 +0600

    capitalize updated

--- string/capitalize.js
+++ string/capitalize.js
@@ -1,8 +1,9 @@
 
 function capStr(string) {
+    let stringSmall = string.toLowerCase();
     let str2 = [];
     let str3;
-    let str = string.split(' ');
+    let str = stringSmall.split(' ');
     for (i = 0; i < str.length; i++) {
         str[i] = str[i][0].toUpperCase() + str[i].substring(1);
         str2.push(str[i]);
@@ -11,4 +12,4 @@ function capStr(string) {
     return str3 = str2.join(' ');
 }
 
-console.log(capStr('Capitalize Every first Letter of each word in a String'));
+console.log(capStr('Capitalize EvEry first LeTter of each worD in a String'));
