36520c094aa6e40632a38cdcdd962fe622b9c7b7
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-02-04 17:31:13 +0600

    pt1 done

---
+++ pt1/rand.js
@@ -0,0 +1,23 @@
+function rand() {
+    return Math.floor(Math.random() * 11) + 10;
+}
+
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
+console.log(rand());
