47812d34cc1749a65715bffdb0a1ba39b810b98a
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-02-04 17:04:50 +0600

    3rd done

---
+++ pt1/vowel.js
@@ -0,0 +1,14 @@
+function vowel(a) {
+    b = a.toLowerCase();
+    let sum = 0;
+    for (let i of b) {
+        if (i === 'a' || i === 'e' || i === 'i' || i === 'o' || i === 'u') {
+            sum += 1;
+        } else {
+            sum += 0;
+        }
+    }
+    return sum;
+}
+
+console.log(vowel('A quick brown fox jumps over the lazy dog.'));
